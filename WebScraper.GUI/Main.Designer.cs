d6f88ddcc2298c1b5fd96384c01d85deebd3ebed
David Bullock <david@invertedtomato.com>
2014-04-13 20:43:16 +1000

    Added icons

--- WebScraper.GUI/Main.Designer.cs
+++ WebScraper.GUI/Main.Designer.cs
@@ -23,6 +23,7 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
 			this.label1 = new System.Windows.Forms.Label();
 			this.txtOutputDir = new System.Windows.Forms.TextBox();
 			this.BtnOutputFile = new System.Windows.Forms.Button();
@@ -242,6 +243,7 @@
 			this.Controls.Add(this.BtnOutputFile);
 			this.Controls.Add(this.txtOutputDir);
 			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Main";
 			this.Text = "Web Scraper";
 			((System.ComponentModel.ISupportInitialize)(this.numDelay)).EndInit();
